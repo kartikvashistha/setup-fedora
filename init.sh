65dcdc5bd3bb71eb64a6accb255f04a538bf3d6e
Kartik Vashistha <kartikv.work@gmail.com>
2023-09-04 00:35:34 +0100

    more packages and comment to init script

--- init.sh
+++ init.sh
@@ -2,9 +2,11 @@
 
 # Bootstrap script that installs the initial ansible dependencies, few base packages, the Oh My ZSH framework and runs the playbook.yml in this repo.
 
-sudo dnf install ansible ansible-core curl git -y
+sudo dnf install ansible ansible-core curl git zsh -y
 
 ansible-galaxy collection install community.general
+
+chsh -s $(which zsh) # this will require reboot for the shell changes to take place
 
 # Run the omz framework setup script
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
