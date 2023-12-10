bca9ac0e05d7c28e4a1316f7e836eb894d382f81
Kartik Vashistha <kartikv.work@gmail.com>
2023-12-10 23:52:30 +0000

    Minor changes

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-#/bin/sh
-
+#/bin/bash
+set -e
 # Bootstrap script that installs the initial ansible dependencies, few base packages, the Oh My ZSH framework and runs the playbook.yml in this repo.
 
-sudo dnf install ansible ansible-core curl git zsh chsh -y
+sudo dnf install ansible ansible-core curl git zsh -y
 
 ansible-galaxy collection install community.general
 
